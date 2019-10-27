8ac3847b46e1fcf159068395306c36795574a6fe
Ali <alice.krouse@gmail.com>
2019-10-27 10:04:24 -0700

    added connection scripts

--- Assets/Scripts/connector.cs
+++ Assets/Scripts/connector.cs
@@ -7,11 +7,16 @@ public class connector : MonoBehaviour
     private SpriteRenderer sr;
 
     public List<GameObject> objectsInRadius;
+    public List<GameObject> connectedObjects;
     public GameObject line;
+
+    public int connectionScore;
+
+    public float maxColorDifference;
 
     void Start()
     {
-        sr = GetComponent<SpriteRenderer>();
+        sr = GetComponentInParent<SpriteRenderer>();
     }
     
     void Update()
@@ -20,6 +25,9 @@ public class connector : MonoBehaviour
         //check if r g and b are each within a certain distance of players values
         //spawn line renderer pointing at connected player
         //line needs a script on it to destroy itself if the colors change
+
+        foreach (GameObject g in objectsInRadius)
+            CheckColorSimilarity(g.GetComponent<SpriteRenderer>());
     }
 
     private void OnTriggerStay2D(Collider2D collision)
@@ -38,5 +46,71 @@ public class connector : MonoBehaviour
             if (objectsInRadius.Contains(collision.gameObject))
                 objectsInRadius.Remove(collision.gameObject);
         }
+    }
+
+    private void CheckColorSimilarity(SpriteRenderer other)
+    {
+        bool red = false;
+        bool green = false;
+        bool blue = false;
+
+        if (Mathf.Abs(sr.color.r - other.color.r) < maxColorDifference)
+            red = true;
+        else
+            red = false;
+
+        if (Mathf.Abs(sr.color.g - other.color.g) < maxColorDifference)
+            green = true;
+        else
+            green = false;
+
+        if (Mathf.Abs(sr.color.b - other.color.b) < maxColorDifference)
+            blue = true;
+        else
+            blue = false;
+
+        if (!connectedObjects.Contains(other.gameObject))
+        {
+            if (red && green && blue)
+                Connect(other.gameObject);
+        }
+
+        if (connectedObjects.Contains(other.gameObject))
+        {
+            if (!red || !green || !blue)
+                Disconnect(other.gameObject);
+        }
+    }
+
+    private void Connect(GameObject other)
+    {
+        connectedObjects.Add(other);
+
+        GameObject lr = Instantiate(line, transform);
+        lr.GetComponent<lineScript>().connectedObject = other.transform;
+
+        TallyScore();
+    }
+
+    public void Disconnect(GameObject other)
+    {
+        connectedObjects.Remove(other);
+
+        TallyScore();
+    }
+
+    private void TallyScore()
+    {
+        int score = 0;
+
+        foreach (GameObject g in connectedObjects)
+        {
+            if (g.CompareTag("AI"))
+                score += 1;
+            if (g.CompareTag("Player"))
+                score += 2;
+        }
+
+        connectionScore = score;
     }
 }
---
+++ Assets/Scripts/lineScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lineScript : MonoBehaviour
+{
+    private LineRenderer lr;
+
+    public Transform connectedObject;
+    private float maxDistance;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+
+        maxDistance = GetComponentInParent<CircleCollider2D>().radius;
+    }
+    
+    void Update()
+    {
+        lr.SetPosition(0, transform.parent.position);
+        lr.SetPosition(1, connectedObject.position);
+
+        if (Vector2.Distance(transform.position, connectedObject.position) > maxDistance)
+        {
+            GetComponentInParent<connector>().Disconnect(connectedObject.gameObject);
+            Destroy(gameObject);
+        }
+        
+        lr.startColor = transform.parent.parent.GetComponent<SpriteRenderer>().color;
+        lr.endColor = connectedObject.GetComponent<SpriteRenderer>().color;
+
+        if (!GetComponentInParent<connector>().connectedObjects.Contains(connectedObject.gameObject))
+            Destroy(gameObject);
+    }
+}
