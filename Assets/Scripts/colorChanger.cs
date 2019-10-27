ea139826bd51e1beeb66b764f5ab43b67d938885
Ali <alice.krouse@gmail.com>
2019-10-27 08:45:12 -0700

    first commit

---
+++ Assets/Scripts/colorChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class colorChanger : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    public Color[] colors;
+    private int goingToColor;
+    private float lerpSpeed, waitTime;
+    public float minLerp, maxLerp, minWait, maxWait;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        SwitchColor();
+    }
+    
+    void Update()
+    {
+        sr.color = Color.Lerp(sr.color, colors[goingToColor], Time.deltaTime * lerpSpeed);
+
+        waitTime -= Time.deltaTime;
+        if (waitTime <= 0)
+            SwitchColor();
+    }
+
+    void SwitchColor()
+    {
+        goingToColor = Random.Range(0, colors.Length);
+        lerpSpeed = Random.Range(minLerp, maxLerp);
+        waitTime = Random.Range(minWait, maxWait);
+    }
+}
---
+++ Assets/Scripts/connector.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class connector : MonoBehaviour
+{
+    private SpriteRenderer sr;
+
+    public List<GameObject> objectsInRadius;
+    public GameObject line;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+    
+    void Update()
+    {
+        //continuously check (maybe not every frame) for similar color
+        //check if r g and b are each within a certain distance of players values
+        //spawn line renderer pointing at connected player
+        //line needs a script on it to destroy itself if the colors change
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("AI"))
+        {
+            if (!objectsInRadius.Contains(collision.gameObject))
+                objectsInRadius.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") || collision.CompareTag("AI"))
+        {
+            if (objectsInRadius.Contains(collision.gameObject))
+                objectsInRadius.Remove(collision.gameObject);
+        }
+    }
+}
